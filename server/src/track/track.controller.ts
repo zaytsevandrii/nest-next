f6c69ce6ba35993f567b4c2acb5ae908ad8d9c9e
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-02-17 20:32:42 +0000

    add file modules and connect with track

--- server/src/track/track.controller.ts
+++ server/src/track/track.controller.ts
@@ -1,16 +1,36 @@
 import { CreateCommentDto } from './dto/create-comment.dto';
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { TrackService } from './track.service';
 import { ObjectId } from 'mongoose';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 
 @Controller('/tracks')
 export class TrackController {
   constructor(private trackService: TrackService) {}
+
   @Post()
-  create(@Body() dto: CreateTrackDto) {
-    return this.trackService.create(dto);
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'picture', maxCount: 1 },
+      { name: 'audio', maxCount: 1 },
+    ]),
+  )
+  create(@UploadedFiles() files, @Body() dto: CreateTrackDto) {
+    const {picture,audio} = files
+    return this.trackService.create(dto,picture[0],audio[0]);
   }
+
+
   @Get()
   getAll() {
     return this.trackService.getAll();
--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -13,7 +13,7 @@ export class TrackService{
     constructor(@InjectModel(Track.name) private trackModel: Model<TrackDocument>,
                 @InjectModel(Comment.name) private commentModel: Model<CommentDocument>){}
 
-    async create(dto:CreateTrackDto):Promise<Track>{
+    async create(dto:CreateTrackDto,picture, audio):Promise<Track>{
         const track = await this.trackModel.create({...dto,listens:0})
         return track
     }
